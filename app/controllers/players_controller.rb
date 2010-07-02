ef2189a95dfc70c2f98253500d60e144b38fd534
Kyle Suttie <suttiebuddy02@aol.com>
2010-07-02 09:42:49 -0400

    Works with ajax, fixing button toggle problem next

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,19 +1,26 @@
 class PlayersController < ApplicationController
   def index
+    #debugger
+      @sorts = ['Name', 'Team', 'AVG', 'HR', 'RBI', 'RUNS', 'SB', 'OPS', 'GAMES']
     if params[:dir] != "ASC" and params[:dir] != "DESC"
       @players = Player.find(:all,
 			     :conditions => "AVG != 'NaN'",
 			     :order => "AVG DESC",
 			     :limit => 25)
+      params[:dir] = "DESC"
+      params[:sort] = "AVG"
+      respond_to do |format|
+        format.html
+      end
     else
       @players = Player.find(:all,
-			     :conditions => ["AVG != 'NaN'", "OPS != 'NaN'"],
+			     :conditions => "AVG != 'NaN' AND OPS != 'NaN'",
 			     :order => params[:sort] + ' ' + params[:dir],
 			     :limit => 25)
-    end
-    respond_to do |format|
-      format.html # index.html.erb
-    end
+      respond_to do |format|
+        format.js
+      end
+    end 
   end
 
   def sort
