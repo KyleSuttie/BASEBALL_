ef3f1e49ac5379f2a0d5fef421a3448ce30f3061
Kyle Suttie <suttiebuddy02@aol.com>
2010-07-08 15:50:24 -0400

    Works, added a little style, no upload yet

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,36 +2,31 @@ class PlayersController < ApplicationController
   def index
       @fields = ['Name', 'Team', 'AVG', 'HR', 'RBI', 'Runs', 'SB', 'OPS', 'Games']
       @conds = "NaN"
+      @conds_two = "AVG != ? AND OPS != ?"
     if params[:dir] != "ASC" and params[:dir] != "DESC"
       params[:page] = "1"
       params[:dir] = "DESC"
       params[:field] = "AVG"
+      @order = params[:field] + ' ' + params[:dir]
       @players = Player.paginate :page => Integer(params[:page]),
-				 :conditions => ["AVG != ? AND OPS != ?", @conds, @conds],
-				 :order => params[:field] + ' ' + params[:dir]
+				 :conditions => [@conds_two, @conds, @conds],
+				 :order => @order
       respond_to do |format|
         format.html
       end
     else
+      @order = params[:field] + ' ' + params[:dir]
       @players = Player.paginate :page => Integer(params[:page]),
-				 :conditions => ["AVG != ? AND OPS != ?", @conds, @conds],
-				 :order => params[:field] + ' ' + params[:dir]
+				 :conditions => [@conds_two, @conds, @conds],
+				 :order => @order
       respond_to do |format|
         format.js
       end
-    end 
+    end
 
   end
 
   def sort
     redirect_to :action => 'index', :field => params[:field], :dir => params[:dir], :page => params[:page]
   end
-
-
-
-  def upload_file
-    post = Player.save(params[:upload])
-    render :text => "File has been uploaded successfully"
-  end
-    
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -2,23 +2,4 @@ class Player < ActiveRecord::Base
   cattr_reader :per_page
   @@per_page = 25
 
-  def self.save(upload)
-    name = upload['datafile'].original_filename
-    directory = "public/baseball"
-    path = File.join(directory, name)
-    File.open(path, "wb") { |f| f.write(upload['datafile'].read) }
-  end
-
-
-
-
-
-
-
-
-  #def file_upload=(field)
-  #  self.name = base_part_of(field.original_filename)
-  #  self.data = field.read
-  #end
-
 end
